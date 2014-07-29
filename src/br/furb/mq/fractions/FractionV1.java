ad9c0bd67e2968545c071a427baa087c19596fe8
Ingmar SA <ingmar.aguiar@gmail.com>
2014-07-29 18:06:41 -0300

    Frações

---
+++ src/br/furb/mq/fractions/FractionV1.java
@@ -0,0 +1,136 @@
+package br.furb.mq.fractions;
+
+/**
+ * The Fraction class implements non-negative fractions, i.e., rational numbers. <br>
+ * Based in the following example: <a href="http://www.cs.berkeley.edu/~jrs/61bf98/labs/lab2/Fraction.java">http://www.cs.berkeley.edu/~jrs/61bf98/labs/lab2/Fraction.java</a>
+ */
+public class FractionV1 {
+	/**
+	 * Constructs a Fraction n/d.
+	 * 
+	 * @param n
+	 *            is the numerator, assumed non-negative.
+	 * @param d
+	 *            is the denominator, assumed positive.
+	 */
+	FractionV1(int n, int d) {
+		numerator = n;
+		denominator = d;
+	}
+
+	/**
+	 * Constructs a Fraction n/1.
+	 * 
+	 * @param n
+	 *            is the numerator, assumed non-negative.
+	 */
+	public FractionV1(int n) {
+		this(n, 1);
+	}
+
+	/**
+	 * Constructs a Fraction 0/1.
+	 */
+	public FractionV1() {
+		numerator = 0;
+		denominator = 1;
+	}
+
+	/**
+	 * Converts this fraction to a string format: "numerator/denominator."
+	 * Fractions are printed in reduced form (part of your assignment is to make
+	 * this statement true).
+	 * 
+	 * @return a String representation of this Fraction.
+	 */
+	public String toString() {
+		int thisGcd = gcd(numerator, denominator);
+
+		return (numerator / thisGcd + "/" + denominator / thisGcd);
+	}
+
+	/**
+	 * Calculates and returns the double floating point value of a fraction.
+	 * 
+	 * @return a double floating point value for this Fraction.
+	 */
+	public double evaluate() {
+		double n = numerator; // convert to double
+		double d = denominator;
+		return (n / d);
+	}
+
+	/**
+	 * Add f2 to this fraction and return the result.
+	 * 
+	 * @param f2
+	 *            is the fraction to be added.
+	 * @return the result of adding f2 to this Fraction.
+	 */
+	public FractionV1 add(FractionV1 f2) {
+		FractionV1 r = new FractionV1((numerator * f2.denominator)
+				+ (f2.numerator * denominator), (denominator * f2.denominator));
+		return r;
+	}
+
+	/**
+	 * Computes the greatest common divisor (gcd) of the two inputs.
+	 * 
+	 * @param x
+	 *            is assumed positive
+	 * @param y
+	 *            is assumed non-negative
+	 * @return the gcd of x and y
+	 */
+	static private int gcd(int x, int y) {
+		/* Remove the following line. */
+		return 1;
+	}
+
+	/* private fields within a Fraction. */
+	private int numerator;
+	private int denominator;
+
+	/**
+	 * Put the Fraction class through some test sequences.
+	 * 
+	 * @param argv
+	 *            is not used.
+	 */
+	public static void main(String[] argv) {
+
+		/* Test all three contructors and toString. */
+		FractionV1 f0 = new FractionV1();
+		FractionV1 f1 = new FractionV1(3);
+		FractionV1 f2 = new FractionV1(12, 20);
+
+		System.out.println("\nTesting constructors (and toString):");
+		System.out.println("The fraction f0 is " + f0.toString());
+		System.out.println("The fraction f1 is " + f1); // toString is implicit
+		System.out.println("The fraction f2 is " + f2);
+
+		/* Test methods on Fraction: add and evaluate. */
+		System.out.println("\nTesting add and evaluate:");
+		System.out.println("The floating point value of " + f1 + " is "
+				+ f1.evaluate());
+		System.out.println("The floating point value of " + f2 + " is "
+				+ f2.evaluate());
+
+		/*
+		 * Fraction sumOfTwo = _______________; Fraction sumOfThree =
+		 * ______________;
+		 * 
+		 * System.out.println("The sum of " + f1 + " and " + f2 + " is " +
+		 * sumOfTwo); System.out.println("The sum of " + f0 + ", " + f1 +
+		 * " and " + f2 + " is " + sumOfThree);
+		 */
+
+		/* Test gcd function (static method). */
+		System.out.println("\nTesting gcd:");
+		System.out.println("The gcd of 2 and 10 is: " + gcd(2, 10));
+		System.out.println("The gcd of 15 and 5 is: " + gcd(15, 5));
+		System.out.println("The gcd of 24 and 18 is: " + gcd(24, 18));
+		System.out.println("The gcd of 10 and 10 is: " + gcd(10, 10));
+		System.out.println("The gcd of 21 and 400 is: " + gcd(21, 400));
+	}
+}
